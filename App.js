3a599ff3140f85a8156c21d2245290cbcff400f0
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2023-03-30 12:50:26 +0500

    добавил индикатор загрузки к кнопке при отпрпавки формы

--- App.js
+++ App.js
@@ -1,31 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { ActivityIndicator, Button } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RegistatrionPage } from "./src/pages/RegistationPage/RegistatrionPage";
 import { SignIn } from "./src/pages/SignIn/SignIn";
+import { useFonts } from "expo-font";
+import { COLORS } from "./src/constants/Colors/Colors";
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const [fontsLoaded] = useFonts({
+    "Roboto-flex": require("./assets/fonts/RobotoFlex-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <ActivityIndicator animating={true} color={COLORS.Accent} />;
+  }
 
   return (
-    <NavigationContainer>
-      {/* initialRouteName изменить на SignIn */}
-      <Stack.Navigator initialRouteName="SignIn">
-        {/* Начальная страница(Страница входа) ее пилит олександер */}
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{ title: "Страница Входа", headerShown: false }}
-        />
-        {/* Страница регистрации */}
-        <Stack.Screen
-          name="SignOut"
-          component={RegistatrionPage}
-          options={{ title: "Страница Входа", headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    fontsLoaded && (
+      <NavigationContainer>
+        {/* initialRouteName изменить на SignIn */}
+        <Stack.Navigator initialRouteName="SignIn">
+          {/* Начальная страница(Страница входа) ее пилит олександер */}
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ title: "Страница Входа", headerShown: false }}
+          />
+          {/* Страница регистрации */}
+          <Stack.Screen
+            name="SignOut"
+            component={RegistatrionPage}
+            options={{ title: "Страница Входа", headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   );
 }
 
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
+import { COLORS } from "../../constants/Colors/Colors";
 
 export const SignIn = ({ navigation }) => {
   return (
     <View>
       <Text>SignIn</Text>
       <Button onPress={() => navigation.navigate("SignOut")}>
-        перейти к Регистрации
+        <Text
+          style={{
+            fontFamily: "Roboto-flex",
+            fontWeight: "bold",
+            color: COLORS.Accent,
+            fontSize: 20,
+          }}
+        >
+          перейти к Регистрации
+        </Text>
       </Button>
     </View>
   );
